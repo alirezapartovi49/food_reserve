2aa1545d4fbd4d2cf876920e3052e05e0b5f3fb8
zparto82 <zparto82@gmail.com>
2025-05-25 14:34:43 +0330

    customize viewset for self route

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
-from rest_framework import viewsets
 from rest_framework.views import APIView
+from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import RetrieveUpdateAPIView, DestroyAPIView
@@ -31,8 +31,14 @@ class UserProfileAPIView(RetrieveUpdateAPIView):
         )
 
 
-class SelfViewSet(viewsets.ModelViewSet):
-    """Self's routes with IsAdminOrReadOnly permision"""
+class SelfViewSet(
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Self's routes with IsAdminOrReadOnly permision
+    it has update delete and get actions
+    """
 
     serializer_class = SelfSerializer
     permission_classes = (
